dde6dd37b11b5f43ee76c87b316985927073ffe2
Laf111 <47532310+Laf111@users.noreply.github.com>
2022-08-05 14:27:14 +0200

    V12
    2022/08/05 V12 :    
        > WUT developpement is interrupted (too many buggs with no way to debug), so no more channel version for V12 and up
          If you continue to use the previous channel version, avoid transfer/operation on the SDCard (CRC errors, crashes)
        > much more stable version using dynamic libs (like the original software), fix : 
            - transfers number limitation on the SDCard, now 8 is allowed
            - CRC errors on SDCard
          and i suppose : 
            - Disconnects to ftp server while copying a injected game #22 
            - Crash when trying to access a folder on SD #23 
            - unstable on tiramisu #24
            - It seems that using FileZilla client to delete files on sdcard caused it died. #26 
        > multi transfers enhancement : 
            - now connections are re-used (no more alloc/free when transferring files)
            - transfer buffer and thread are preallocated
        > fix : /storage_usb sometimes inaccessible if two USB devices are plugged in #25 
        > fix : renaming file is broken #27

--- src/ftp.c
+++ src/ftp.c
@@ -1388,54 +1388,6 @@ static int32_t ftp_LIST(connection_t *connection, char *path) {
     return result;
 }
 
-
-// You must free the result if result is non-NULL.
-static char *str_replace(char *orig, char *rep, char *with) {
-    char *result; // the return string
-    char *ins;    // the next insert point
-    char *tmp;    // varies
-    int len_rep;  // length of rep (the string to remove)
-    int len_with; // length of with (the string to replace rep with)
-    int len_front; // distance between rep and end of last rep
-    int count;    // number of replacements
-
-    // sanity checks and initialization
-    if (!orig || !rep)
-        return NULL;
-    len_rep = strlen(rep);
-    if (len_rep == 0)
-        return NULL; // empty rep causes infinite loop during count
-    if (!with)
-        with = "";
-    len_with = strlen(with);
-
-    // count the number of replacements needed
-    ins = orig;
-    for (count = 0; (tmp = strstr(ins, rep)); ++count) {
-        ins = tmp + len_rep;
-    }
-
-    tmp = result = malloc(strlen(orig) + (len_with - len_rep) * count + 1);
-
-    if (!result)
-        return NULL;
-
-    // first time through the loop, all the variable are set correctly
-    // from here on,
-    //    tmp points to the end of the result string
-    //    ins points to the next occurrence of rep in orig
-    //    orig points to the remainder of orig after "end of rep"
-    while (count--) {
-        ins = strstr(orig, rep);
-        len_front = ins - orig;
-        tmp = strncpy(tmp, orig, len_front) + len_front;
-        tmp = strcpy(tmp, with) + len_with;
-        orig += len_front + len_rep; // move to next "end of rep"
-    }
-    strcpy(tmp, orig);
-    return result;
-}
-
 static int32_t ftp_RETR(connection_t *connection, char *path) {
 
     char *folder = NULL;
@@ -1447,7 +1399,6 @@ static int32_t ftp_RETR(connection_t *connection, char *path) {
 
     secureAndSplitPath(connection->cwd, path, &folder, &fileName);
     strcat(folder, "/");
-    char *filePath = to_real_path(folder, fileName);
 
     strcpy(connection->fileName, fileName);
 	strcpy(connection->cwd, folder);
